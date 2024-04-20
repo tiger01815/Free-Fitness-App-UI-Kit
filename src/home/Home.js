78a0b4b012f92e545aadaeec0bd9520e5788a269
tiger94815.com <tiger94815@gmail.com>
2024-04-20 10:55:50 +0900

    2024/04/20

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,20 +1,39 @@
 import react from 'react';
 import {
     Text,
-    View
+    View,
+    StyleSheet,
+    Image,
+    TouchableOpacity
 } from 'react-native';
 
 import CommonHeader from '../common/CommonHeader';
+import Constants from '../common/Constants';
 
 const HomeScreen = ({navigation})=>{
     const onMenuPress = () => navigation.openDrawer()
     return(
         <View
-            style={{
-                paddingHorizontal:25
-            }}
+            style={styles.container}
         >
-            <CommonHeader
+            <View style={styles.header}>
+                <View>
+                    <Text style={{color:"#ADA4A5", fontFamily:Constants.FONT_FAMILY.PRIMARY_REGULAR, fontSize:Constants.FONT_SIZE.FT12}}>Welcome Back</Text>
+                    <Text style={{color:'black', fontSize:Constants.FONT_SIZE.FT22, fontFamily:Constants.FONT_FAMILY.PRIMARY_BOLD}}>Stefani Wong</Text>
+                </View>
+                <TouchableOpacity
+                    style={styles.headernotificaionconatiner}
+                >
+                    <Image
+                        source={require('../../assets/image/notification.png')}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.firstsection}>
+                <View style={styles.cone}/>
+                <Text>11</Text>
+            </View>
+            {/* <CommonHeader
                 title={'Home'}
                 onMenuPress={onMenuPress}
             />
@@ -24,9 +43,47 @@ const HomeScreen = ({navigation})=>{
                 }}
             >
                 This is Home Screen.
-            </Text>
+            </Text> */}
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'white',
+        paddingHorizontal:25,
+        flex:1
+    },
+    header:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center'
+    },
+    headernotificaionconatiner:{
+        width:40,
+        height:40,
+        borderRadius:8,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#F7F8F8'
+    },
+    firstsection:{
+        height:300,
+        // width:'100%'
+        width:300
+
+    },
+    cone:{
+        width: 0,
+        height: 0,
+        borderLeftWidth: 55,
+        borderLeftColor: "transparent",
+        borderRightWidth: 55,
+        borderRightColor: "transparent",
+        borderTopWidth: 100,
+        borderTopColor: "red",
+        borderRadius: 55,
+    }
+})
 
 export default HomeScreen;
